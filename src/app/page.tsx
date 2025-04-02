d70b283c241a3cf1cd513b5578479a3ca8ffa0ce
ArtashesMar <artashesmargaryanseryojayi@gmail.com>
2025-04-02 17:39:39 +0400

    minor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { useEffect, useState } from "react";
-import fetchData from "@/lib/fetchData";
 import Card from "@/components/Card";
+import fetchData from "@/lib/fetchData";
+import { useEffect, useState } from "react";
 
 const ITEMS_PER_PAGE = 16;
 
